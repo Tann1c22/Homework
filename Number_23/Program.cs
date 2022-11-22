6d0638f64b39475e8c580900772e598b004b4261
Tann1c22 <iborovkov804@gmail.com>
2022-11-22 20:41:29 +0400

    Third Homework

---
+++ Number_23/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int num = int.Parse(Console.ReadLine());
+if(num > 0){
+    for (int i = 0; i <= num; i++)
+    {
+        int result = i*i*i;
+        Console.WriteLine(result);
+    }
+} else if (num < 0)
+{
+    for (int i = 0; i >= num; i--)
+    {
+        int result = i*i*i;
+        Console.WriteLine(result);
+    }
+} else {
+    Console.WriteLine("0");
+}
+    
+
